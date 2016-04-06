0bfaab403668d2f85bd14604d955d0d3ec00eeb0
hannut91 <hannut91@gmail.com>
2016-04-06 11:05:13 +0900

    url change

--- manage/urls.py
+++ manage/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'stat/',views.CheckState.as_view(), name='check_stat'),
     url(r'pingstate/',views.CheckPing.as_view(), name="check_ping"),
-    url(r'sendgit/',views.SendGit.as_view(),name="send_git"),
+    url(r'sendgit',views.SendGit.as_view(),name="send_git"),
 ]
