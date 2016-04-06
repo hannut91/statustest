fa44eba948609013f7dfc1c8e85c24838f9a14ba
hannut91 <hannut91@gmail.com>
2016-04-06 12:13:05 +0900

    add log setting

--- status/settings.py
+++ status/settings.py
@@ -125,3 +125,19 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CSRF_COOKIE_SECURE = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
